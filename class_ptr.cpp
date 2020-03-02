f7d04b10f8a1ba0f3d6364958782eb6d2087ecbb
mateusz <mateusz.kojro@unilodz.eu>
2020-03-02 21:46:56 +0100

    pozostale pliki do 1 wyklad 1

--- class_ptr.cpp
+++ class_ptr.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 
+class point{
+public:
+  int x;
+  int y;
+
+  void do_sth();
+};
+
 int main(){
-  
+point a;
+point* b = new point;
+
+b = &a;
+b->x = 10;
+b->y =11;
+
+printf("%d\t%d\n",b->x,b->y );
+
+
+}
+
+void point::do_sth(){
+  printf("taki sobie tekst\n");
 }
